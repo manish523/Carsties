c6bc986afbc7f06ade44070366956061fc275d2e
manish523 <manish.callme@gmail.com>
2024-04-26 18:19:20 +0530

    Added front end next js app with basic navbar  auction listing some layout changes and countdown timer

---
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AuctionCard from "./AuctionCard";
+
+async function getData() {
+  const res = await fetch("http://localhost:6001/search?pageSize=10");
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  return res.json();
+}
+{
+  /* {JSON.stringify(data, null, 2)} */
+}
+export default async function Listings() {
+  const data = await getData();
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      {data &&
+        data.results.map((auction: any) => (
+          <AuctionCard auction={auction} key={auction.id} />
+        ))}
+    </div>
+  );
+}
