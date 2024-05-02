9986a5eabca5ce79d0e9b32a95fc2081943df467
manish523 <manish.callme@gmail.com>
2024-05-02 13:55:49 +0530

    configured identity server inside next auth

---
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import DuendeIdentityServer6 from "next-auth/providers/duende-identity-server6";
+
+// const handler = NextAuth({
+//   ...
+// })
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    DuendeIdentityServer6({
+      id: "id-server",
+      clientId: "nextApp",
+      clientSecret: "secret",
+      issuer: "http://localhost:5000",
+      authorization: { params: { scope: "openid profile auctionApp" } },
+      idToken: true,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
