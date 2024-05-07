0058341671a59fe137d8e9ddafd7adeb62bda054
manish523 <manish.callme@gmail.com>
2024-05-07 15:51:08 +0530

    finished client side identity with next auth with login and gettoken functionalities

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -19,6 +19,24 @@ export const authOptions: NextAuthOptions = {
       idToken: true,
     }),
   ],
+  callbacks: {
+    async jwt({ token, profile, account, user }) {
+      // console.log({ token, profile, account, user });
+      if (profile) {
+        token.username = profile.username;
+      }
+      if (account) {
+        token.access_token = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.username = token.username;
+      }
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,15 +1,23 @@
-'use client'
+"use client";
 
 import { useParamsStore } from "@/hooks/useParamsStore";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { AiOutlineCar } from "react-icons/ai";
 
 export default function Logo() {
   const reset = useParamsStore((x) => x.reset);
+  const router = useRouter();
+  const pathName = usePathname();
+
+  function doReset() {
+    if (pathName !== "/") router.push("/");
+    reset();
+  }
 
   return (
     <div
-      onClick={reset}
+      onClick={doReset}
       className="cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500"
     >
       <AiOutlineCar size={34}></AiOutlineCar>
